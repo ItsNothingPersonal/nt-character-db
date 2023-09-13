e4cec926ef2cf99b51cc7f0dbe9c9166a8ec440d
Sebastian Tremmel <littleperlmonk@gmail.com>
2023-09-13 22:30:42 +0200

    verlinkungg zu disziplinen hinzugefügt

--- src/lib/components/discipline.svelte
+++ src/lib/components/discipline.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 	import type { DisciplineName } from '$lib/zod/disciplineName';
-	import { Label, P } from 'flowbite-svelte';
+	import { A, P } from 'flowbite-svelte';
 
 	export let name: DisciplineName;
 	export let disciplineValue: number;
 </script>
 
-<div class="flex flex-col">
-	<Label color="red" class="flex items-center font-bold gap-2">
-		{name}
-		<P size="2xl">{disciplineValue}</P>
-	</Label>
+<div class="grid grid-rows-1 grid-cols-2">
+	<A href="https://vamp.bynightstudios.com/vampire/disciplines/{name.toLowerCase()}">
+		<P color="text-red-700 dark:text-red-500" weight="bold" size="sm">{name}</P>
+	</A>
+	<P size="2xl">{disciplineValue}</P>
 </div>
