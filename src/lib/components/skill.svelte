ca0a3e6ba25dbcfe4540a696a6595004db5f36c8
Sebastian Tremmel <littleperlmonk@gmail.com>
2023-09-13 23:23:07 +0200

    touchscreen erkennung für spezialisierungen bei skills eingebaut
    - Die Tatsache ob ein Touchscreen verwendet wird oder nicht wird nun nicht mehr rudimentär über die Viewportbreite sondern via javascript ermittelt

--- src/lib/components/skill.svelte
+++ src/lib/components/skill.svelte
@@ -1,45 +1,38 @@
 <script lang="ts">
-	import { ScreenSize } from '$lib/sceenSize';
+	import { detectTouchscreen } from '$lib/util';
 	import type { SkillName } from '$lib/zod/skillName';
 	import { A, P, Tooltip } from 'flowbite-svelte';
 	import { LinkSolid } from 'flowbite-svelte-icons';
+	import { onMount } from 'svelte';
 
 	export let name: SkillName;
 	export let value: number;
 	export let specialization: string | undefined;
 
-	let innerWidth: number;
+	let isTouchscreen = false;
 
-	$: small = innerWidth < ScreenSize.LG;
+	onMount(() => {
+		isTouchscreen = detectTouchscreen();
+	});
 </script>
 
-<svelte:window bind:innerWidth />
-
 <div class="grid grid-cols-1 auto-rows-auto max-h-fit">
-	{#if small}
-		<div class="grid grid-rows-1 grid-cols-2 gap-9">
+	<div class="grid grid-rows-1 grid-cols-2">
+		{#if isTouchscreen}
 			<div class="flex items-center gap-1">
 				<A href="https://vamp.bynightstudios.com/vampire/skills/{name.toLowerCase()}">
 					<LinkSolid size="sm" role="link" />
 				</A>
 				<P color="text-red-700 dark:text-red-500" weight="bold" size="sm">{name}</P>
 			</div>
-			<P size="2xl">{value}</P>
-		</div>
-		<P>{specialization ?? ''}</P>
-	{:else}
-		<div class="grid grid-rows-1 grid-cols-2">
+		{:else}
 			<A href="https://vamp.bynightstudios.com/vampire/skills/{name.toLowerCase()}">
-				<div class="flex">
-					<div class="flex items-center">
-						<P color="text-red-700 dark:text-red-500" weight="bold" size="sm">{name}</P>
-					</div>
-				</div>
+				<P color="text-red-700 dark:text-red-500" weight="bold" size="sm">{name}</P>
 			</A>
-			<P size="2xl">{value}</P>
-		</div>
-		{#if specialization}
-			<Tooltip placement="bottom-start">{specialization}</Tooltip>
 		{/if}
+		<P size="2xl">{value}</P>
+	</div>
+	{#if specialization}
+		<Tooltip placement="bottom-start">{specialization}</Tooltip>
 	{/if}
 </div>
--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,3 +1,7 @@
 export function isNullOrUndefined<T>(obj: T | null | undefined): obj is null | undefined {
 	return typeof obj === 'undefined' || obj === null;
 }
+
+export function detectTouchscreen() {
+	return 'ontouchstart' in window;
+}
