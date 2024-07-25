27c7f3c7c3ec9a4193b276b15f28acd6a5744955
Sebastian Tremmel <littleperlmonk@gmail.com>
2024-07-26 01:18:34 +0200

    Quality-Beschreibung bei Items im Charaktersheet
    - Items im Charaktersheet zeigen jetzt analog zum Kampfsheet die Beschreibung der Qualities als Hilfstext an

--- src/lib/components/lotn/characterSheet/charactersheet-elements/items.svelte
+++ src/lib/components/lotn/characterSheet/charactersheet-elements/items.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
-	import ValueNumber from '$lib/components/valueNumber/valueNumber.svelte';
 	import type { PlayerItem } from '$lib/zod/lotn/playerCharacter/playerItem';
+	import { getItemQualityDescription } from '../../util/itemUtil';
+	import HelpText from '../components/HelpText.svelte';
 
 	export let items: PlayerItem[];
 </script>
 
-<div
-	class="grid auto-rows-auto grid-cols-2 gap-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-6"
->
+<div class="grid auto-rows-auto grid-cols-2 gap-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-5">
 	{#each items as item}
-		<ValueNumber label={item.name} specialization={item.quality} />
+		<label
+			class="card label grid h-full w-full auto-rows-auto grid-cols-1 rounded-sm p-4"
+			for={item.name}
+		>
+			<span id={item.name} class="font-bold">{item.name}</span>
+			{#if getItemQualityDescription(item.quality, item.type)}
+				<HelpText id={item.name}>
+					<span class="text-sm">{item.quality}</span>
+					<svelte:fragment slot="helpText">
+						<p class="whitespace-pre-line">
+							{getItemQualityDescription(item.quality, item.type)}
+						</p>
+					</svelte:fragment>
+				</HelpText>
+			{:else}
+				<p class="text-sm">{item.quality}</p>
+			{/if}
+		</label>
 	{/each}
 </div>
